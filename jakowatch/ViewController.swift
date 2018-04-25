5b6553e863c366ebec5758bd69d304c0a89e5e00
勝村里佳 <shengcunlijia@katsumurarikanomacbook-pro.local>
2018-04-25 14:01:22 +0900

    Initial Commit

---
+++ jakowatch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jakowatch
+//
+//  Created by 勝村里佳 on 2018/04/25.
+//  Copyright © 2018年 jako. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jakowatchTests/jakowatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  jakowatchTests.swift
+//  jakowatchTests
+//
+//  Created by 勝村里佳 on 2018/04/25.
+//  Copyright © 2018年 jako. All rights reserved.
+//
+
+import XCTest
+@testable import jakowatch
+
+class jakowatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
